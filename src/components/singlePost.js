52d099ee734e2603f0cc3157b6c04842dea3594d
MalikaN <indee89@gmail.com>
2018-01-24 21:37:55 +0530

    singlepost component  created

---
+++ src/components/singlePost.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+class singlePost extends Component{
+    render(){
+        return(
+            <div>
+            single post
+            </div>
+        )
+    }
+}
+export default singlePost;
